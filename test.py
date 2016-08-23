d7c7ad521145b0e2ce2b20e0b99694fac52adb1f
spirochaete <zz@email.tg>
2016-08-22 20:56:47 -0400

    This is a test comment for the commit

---
+++ test.py
@@ -0,0 +1,23 @@
+def sign(num):
+	if num > 0: 
+		return 1
+	elif  num ==0:
+		return 0
+	else:
+		return -1
+
+print sign(3)
+print sign(-9)
+
+def greet():
+	return 'hi'
+
+temp = greet()
+print temp
+def test(num):
+	if num == 8:
+		return 8
+	else:
+		return 'fail'
+
+print test(5)
